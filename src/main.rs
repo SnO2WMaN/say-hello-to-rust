d1033318dc1d95db3edc8e016372d74ce6d2ea99
SnO2WMaN <me@sno2wman.net>
2022-06-04 20:20:28 +0900

    数宛ゲーム：The Rust Programming Languageによる

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hello, world! 2");
+    let secret: i32 = rand::thread_rng().gen_range(1..=100);
+    println!("secret: {}", secret);
+
+    loop {
+        println!("input?:");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Cannot read line");
+
+        let guess = match guess.trim().parse::<i32>() {
+            Ok(n) => n,
+            Err(_) => continue,
+        };
+
+        println!("answer: {}", guess);
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("equal");
+                break;
+            }
+        }
+    }
 }
